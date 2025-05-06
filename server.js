948e437adbf13ef2edf67d157f6d276789c9f204
bhautikdudhatKmsoft <bhautikdudhat@kmsoft.org>
2025-05-06 16:48:58 +0530

    server

--- server.js
+++ server.js
@@ -1,41 +1,5 @@
-// const express = require('express');
-// const cors = require('cors');
-// const socketIo = require('socket.io');
-// const app = express();
-// const port = 7410;
-
-// app.use(cors({ origin: '*' }));
-// app.use(express.static(__dirname + '/public'));
-
-// const server = app.listen(port, '0.0.0.0', () => {
-//     console.log(`Server started at http://192.168.29.56:${port}`);
-// });
-
-// const io = socketIo(server);
-
-// io.on('connection', (socket) => {
-//     console.log('a user connected');
-
-//     socket.on('offer', (offer) => {
-//         socket.broadcast.emit('offer', offer);
-//     });
-
-//     socket.on('answer', (answer) => {
-//         socket.broadcast.emit('answer', answer);
-//     });
-
-//     socket.on('ice-candidate', (candidate) => {
-//         socket.broadcast.emit('ice-candidate', candidate);
-//     });
-
-//     socket.on('disconnect', () => {
-//         console.log('user disconnected');
-//     });
-// });
-
 const express = require('express');
 const cors = require('cors');
-const http = require('http');
 const socketIo = require('socket.io');
 const app = express();
 const port = 7410;
@@ -43,14 +7,11 @@ const port = 7410;
 app.use(cors({ origin: '*' }));
 app.use(express.static(__dirname + '/public'));
 
-const server = http.createServer(app);
-const io = socketIo(server, {
-    maxHttpBufferSize: 1e8, // 100MB
-});
-
-server.listen(port, '0.0.0.0', () => {
+const server = app.listen(port, '0.0.0.0', () => {
     console.log(`Server started at http://192.168.29.56:${port}`);
 });
+
+const io = socketIo(server);
 
 io.on('connection', (socket) => {
     console.log('a user connected');
@@ -65,11 +26,6 @@ io.on('connection', (socket) => {
 
     socket.on('ice-candidate', (candidate) => {
         socket.broadcast.emit('ice-candidate', candidate);
-    });
-
-    socket.on('file-chunk', (data) => {
-        // Relay chunk to other clients
-        socket.broadcast.emit('file-chunk', data);
     });
 
     socket.on('disconnect', () => {
